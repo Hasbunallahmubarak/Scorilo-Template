e3a397826e6ea0441811a818cb4382a53669740e
Hasbunallahmubarak <82613627+Hasbunallahmubarak@users.noreply.github.com>
2023-09-11 23:49:48 +0100

    Add files via upload

---
+++ Js/scorilo.js
@@ -0,0 +1,8 @@
+function getN() {
+	var nav = document.getElementById('play');
+	if (play.style.display === "block" ) {
+		play.style.display = "none";
+	} else {
+		play.style.display = "block";
+	}
+}
